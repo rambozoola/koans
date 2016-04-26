12de0b069162da98da660efd79a5a01a74794a6a
Ram Bozoola <fb@c-lehmann.de>
2016-04-26 14:36:51 +0200

    Heading towards enlightenment.

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,15 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if a == b and b == c
+    :equilateral
+  else
+    if a == b or b == c or a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
